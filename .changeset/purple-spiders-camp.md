7bf3703ddead36f34641d91d89851d3bf9682b97
Keenest Callas <admin@elsikora.com>
2024-03-17 18:44:27 +0300

    Update build script and refactor path structures
    This commit updates the build script to include "tsc-alias" for better TypeScript alias management. It also refactors the path structure, moving the 'ParameterStoreConfigModule' export to point from "@/config.module" to "@modules/config/config.module" to improve code structure and readability. A new dependency 'tsc-alias' has been added to the package.json file as well.

---
+++ .changeset/purple-spiders-camp.md
@@ -0,0 +1,5 @@
+---
+"@elsikora/nestjs-aws-parameter-store-config": patch
+---
+
+Update build script and refactor path structures
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { ParameterStoreConfigModule } from "@/config.module";
 export { ParameterStoreConfigService } from "@/modules/config/config.service";
+export { ParameterStoreConfigModule } from "@modules/config/config.module";
 export { PARAMETER_STORE_CONFIG_PARAMETERS } from "@shared/constant/config/constant";
